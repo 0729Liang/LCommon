7f3980a51dcdebf1d4c8d02e3c9db788c5006617
1634475153@qq.com <1634475153@qq.com>
2019-01-16 15:12:31 +0800

    修复滑动进度条点击无法定位bug

--- app/src/main/java/com/liang/lcommon/view/LCircleSeekBar.java
+++ app/src/main/java/com/liang/lcommon/view/LCircleSeekBar.java
@@ -252,16 +252,16 @@ public class LCircleSeekBar extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                updateProgress(event, x, y);
                 if (mStartListener != null) {
                     mStartListener.onLSeekBarStart(this);
                 }
-                updateProgress(event, x, y);
                 break;
             case MotionEvent.ACTION_UP:
+                updateProgress(event, x, y);
                 if (mStopListener != null) {
                     mStopListener.onLSeekBarStop(this);
                 }
-                updateProgress(event, x, y);
                 break;
             case MotionEvent.ACTION_MOVE:
                 // 控制最大转一圈，暂时没想到更好的处理方法
