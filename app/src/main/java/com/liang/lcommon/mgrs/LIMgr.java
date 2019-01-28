062fe2b221fcf590d7212b019b959e1a40ba6dd6
1634475153@qq.com <1634475153@qq.com>
2019-01-28 15:04:35 +0800

    保存代码

--- app/src/main/java/com/liang/lcommon/mgrs/LIMgr.java
+++ app/src/main/java/com/liang/lcommon/mgrs/LIMgr.java
@@ -7,5 +7,5 @@ import com.liang.lcommon.able.LRecyclable;
  * CreateAt : 13:32 2019/1/17
  * Describe : Manager 类型的工具类需要创建实例才可以使用，在相关声明周期的地方需要做回收
  */
-public interface LIMgr extends LRecyclable {
+interface LIMgr extends LRecyclable {
 }
